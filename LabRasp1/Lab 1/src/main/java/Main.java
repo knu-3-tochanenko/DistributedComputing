b2deb2252ab3d37284b7ed807c6533064bf3988f
Vladislav <itochanenko@gmail.com>
2020-01-29 02:41:49 +0200

    Added icon

--- LabRasp1/Lab 1/src/main/java/Main.java
+++ LabRasp1/Lab 1/src/main/java/Main.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
@@ -24,6 +25,7 @@ public class Main extends Application {
 //        root.getStylesheets().add(path);
         Scene scene = new Scene(root);
         stage.setTitle("Lab 1 - Distributed Computing");
+        stage.getIcons().add(new Image(Main.class.getResourceAsStream("icon.png")));
         stage.setScene(scene);
         stage.show();
     }
