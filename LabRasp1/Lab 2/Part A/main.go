931dda312038102d18c8c67ef9ee6987b8ce702f
Vladislav <itochanenko@gmail.com>
2020-02-04 23:55:22 +0200

    Changed matrix to bool 2D array. Added drawMatrix method

--- LabRasp1/Lab 2/Part A/main.go
+++ LabRasp1/Lab 2/Part A/main.go
@@ -6,19 +6,36 @@ import (
 	"time"
 )
 
-const N int = 8
-const M int = 10
+const (
+	// N is number of Rows
+	N int = 8
+	// M is number of Columns
+	M int = 10
+)
 
-func generateMatrix() [N][M]int {
+func generateMatrix() [N][M]bool {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	// this will
-	matrix := [N][M]int{}
-	matrix[r.Intn(N)][r.Intn(M)] = 1
+	matrix := [N][M]bool{}
+	matrix[r.Intn(N)][r.Intn(M)] = true
 	return matrix
+}
+
+func drawMatrix(matrix [N][M]bool) {
+	for i := range matrix {
+		for j := range matrix[i] {
+			if matrix[i][j] {
+				fmt.Print("X")
+			} else {
+				fmt.Print("-")
+			}
+		}
+		fmt.Println()
+	}
 }
 
 func main() {
 	var matrix = generateMatrix()
-	fmt.Println(matrix)
+	drawMatrix(matrix)
 	fmt.Printf("END")
 }
