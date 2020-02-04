f06892183d00544086b815a027568ee7d56b8bcd
Vladislav <itochanenko@gmail.com>
2020-02-04 23:31:42 +0200

    Added matrix generation

--- LabRasp1/Lab 2/Part A/main.go
+++ LabRasp1/Lab 2/Part A/main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
+const N int = 8
+const M int = 10
+
+func generateMatrix() [N][M]int {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	// this will
+	matrix := [N][M]int{}
+	matrix[r.Intn(N)][r.Intn(M)] = 1
+	return matrix
+}
+
 func main() {
-	fmt.Printf("It works!")
+	var matrix = generateMatrix()
+	fmt.Println(matrix)
+	fmt.Printf("END")
 }
