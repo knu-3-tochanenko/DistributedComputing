382e72f421eb2a6c149842d7f4b55367667bf24b
Vladislav <itochanenko@gmail.com>
2020-02-24 20:45:03 +0200

    Added fxml file

--- LabRasp1/Lab 6/Part A/src/main/java/Main.java
+++ LabRasp1/Lab 6/Part A/src/main/java/Main.java
@@ -1,32 +1,31 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
+import java.io.IOException;
 
 public class Main extends Application {
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
+    public void start(Stage stage) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("view.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+//        String path = this.getClass().getResource("style.css").toExternalForm();
+//        root.getStylesheets().add(path);
+        Scene scene = new Scene(root);
+        stage.setTitle("Lab 6 - Part A - Distributed Computing - Game of Live");
+//        stage.getIcons().add(new Image(Main.class.getResourceAsStream("icon.png")));
+        stage.setScene(scene);
+        stage.show();
     }
 }
