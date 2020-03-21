858814884411490fd6d4393b282542991e2abdf4
Vladislav <itochanenko@gmail.com>
2020-03-21 18:13:23 +0200

    Added CPP project

---
+++ LabRasp1/CPP/Lab 8/cmake-build-debug/CMakeFiles/foo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/foo.dir/main.cpp.obj"
+  "foo.exe"
+  "foo.exe.manifest"
+  "foo.pdb"
+  "libfoo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/foo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
