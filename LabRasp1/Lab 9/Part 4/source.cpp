8b82b8b00e5332e8b26735c9adfed3015b6c0929
Vladislav <itochanenko@gmail.com>
2020-04-24 23:02:35 +0300

    Complete Part 4

--- LabRasp1/Lab 9/Part 4/source.cpp
+++ LabRasp1/Lab 9/Part 4/source.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
-
+#include "calculator.h"
 int main()
 {
-    std::cout << "Hello World!\n";
+	auto calculator = new Calculator("index.html");
+
+	calculator->addTask(100);
+	calculator->addTask(500);
+	calculator->addTask(1000);
+	calculator->addTask(1500);
+	calculator->addTask(2000);
+	calculator->addTask(2500);
+	calculator->addTask(3000);
+
+	calculator->run();
 }
