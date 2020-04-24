569ee7ef8e26bf63231ae8efc8e457d4ab4ef6b9
Vladislav <itochanenko@gmail.com>
2020-04-24 21:23:21 +0300

    Add logic of Stripes algorithm. Add TBB library

---
+++ LabRasp1/Lab 9/Part 3/calculator.cpp
@@ -0,0 +1,65 @@
+#include "calculator.h"
+
+Calculator::Calculator(std::string path)
+{
+	builder = new HtmlBuilder(path);
+	builder->createHtml()->addHead()->startBody()->createTable();
+
+	sizes = std::vector<int>();
+}
+
+void Calculator::addTask(int size)
+{
+	sizes.push_back(size);
+}
+
+void Calculator::run()
+{
+	double sequentialTime;
+	double time, acceleration;
+
+	for (auto i : sizes) {
+		std::cout << "Calculating matrix size of " << i << " ..." << std::endl;
+		std::vector<std::pair<double, double>> results = std::vector<std::pair<double, double>>();
+		sequentialTime = calculate(i, 1);
+
+		std::cout << "\t1 core\t" << sequentialTime << std::endl;
+
+		time = calculate(i, 2);
+		acceleration = sequentialTime / time;
+		results.push_back(std::pair<double, double>(time, acceleration));
+		std::cout << "\t2 cores\t" << time << std::endl;
+
+		time = calculate(i, 4);
+		acceleration = sequentialTime / time;
+		results.push_back(std::pair<double, double>(time, acceleration));
+		std::cout << "\t4 cores\t" << time << std::endl;
+
+		builder->addResult(i, sequentialTime, results);
+
+		std::cout << i << std::endl;
+	}
+}
+
+double Calculator::calculate(int size, int threadsNumber)
+{
+	auto matrixGenerator = MatrixGenerator(size, 100.0);
+	auto A = matrixGenerator.generate();
+	auto B = matrixGenerator.generate();
+
+	auto stripesShema = StripesDivider(A, B, size, size, threadsNumber);
+
+	auto start = std::chrono::steady_clock::now();
+
+	auto C = stripesShema.calculateProduct();
+
+	auto end = std::chrono::steady_clock::now();
+	std::chrono::duration<double> elapsedSeconds = end - start;
+
+	return elapsedSeconds.count();
+}
+
+void Calculator::finish()
+{
+	builder->finishTable()->finishBody()->finishHtml();
+}
