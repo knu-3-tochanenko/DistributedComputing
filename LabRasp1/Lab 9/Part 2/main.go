a7b7a139117a6db10be42015f36ab6ece2ef9e0c
Vladislav <itochanenko@gmail.com>
2020-04-24 08:34:32 +0300

    Complete Part 2

--- LabRasp1/Lab 9/Part 2/main.go
+++ LabRasp1/Lab 9/Part 2/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
+	"strconv"
 	"sync"
 	"time"
 )
@@ -91,14 +93,113 @@ func calculate(size, threadsNumber int) int64 {
 	return (finishTime - startTime) / 1000000
 }
 
-func main() {
-	fmt.Println(calculate(256, 1), " on size 256 and 1 core")
-	fmt.Println(calculate(512, 1), " on size 512 and 1 core")
-	fmt.Println(calculate(1024, 1), " on size 1024 and 1 core")
-	// fmt.Println(calculate(2048, 1), " on size 2048 and 1 core")
+func startHTML(name string) *os.File {
+	f, err := os.Create(name + ".html")
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
 
-	fmt.Println(calculate(256, 4), " on size 256 and 4 cores")
-	fmt.Println(calculate(512, 4), " on size 512 and 4 cores")
-	fmt.Println(calculate(1024, 4), " on size 1024 and 4 cores")
-	// fmt.Println(calculate(2048, 4), " on size 2048 and 4 cores")
+	f.WriteString(`<html lang="en">
+	<head>
+		<title>Sequential Algorithm</title>
+		<style>
+			body {
+				font-family: Calibri;
+			}
+
+			table, th, td {
+				border: 1px solid black;
+				border-collapse: collapse;
+				padding: 8px;
+			}
+		</style>
+	</head>
+	<body>
+	<table>
+    <tr>
+        <th rowspan="3">Matrix size</th>
+        <th rowspan="3">Sequential algorithm</th>
+        <th colspan="4">Parallel algorithm</th>
+    </tr>
+    <tr>
+        <th colspan="2">2 processes</th>
+        <th colspan="2">4 processes</th>
+    </tr>
+    <tr>
+        <th>Time</th>
+        <th>Acceleration</th>
+        <th>Time</th>
+        <th>Acceleration</th>
+	</tr>`)
+
+	return f
+}
+
+func printRes(f *os.File, matrixSize int, sequentialTime float32, results []FloatPair) {
+	resultString := "<tr>"
+	resultString += "<td>" + strconv.Itoa(matrixSize) + "</td>"
+	resultString += "<td>" + fmt.Sprintf("%f", sequentialTime) + "</td>"
+	for i := range results {
+		resultString += "<td>" + fmt.Sprintf("%f", results[i].first) + "</td>"
+		resultString += "<td>" + fmt.Sprintf("%f", results[i].second) + "</td>"
+	}
+	resultString += "</tr>"
+	_, err := f.WriteString(resultString)
+
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+}
+
+func closeHTML(f *os.File) {
+	_, err := f.WriteString("\n\t</body>\n</html>")
+
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+// FloatPair of a pair of float
+type FloatPair struct {
+	first, second float32
+}
+
+func test(name string) {
+	sizes := []int{100, 500, 1000, 1500, 2000, 2500, 3000}
+	var sequentialTime float32
+	var time, acceleration float32
+
+	f := startHTML(name)
+
+	for i := range sizes {
+		results := make([]FloatPair, 2)
+		sequentialTime = (float32)(calculate(sizes[i], 1)) / 1000000.0
+
+		time = (float32)(calculate(sizes[i], 2)) / 1000000.0
+		acceleration = sequentialTime / time
+		results[0] = FloatPair{time, acceleration}
+
+		time = (float32)(calculate(sizes[i], 4)) / 1000000.0
+		acceleration = sequentialTime / time
+		results[1] = FloatPair{time, acceleration}
+
+		printRes(f, sizes[i], sequentialTime, results)
+		fmt.Println(sizes[i])
+	}
+	closeHTML(f)
+}
+
+func main() {
+	test("index")
 }
