3a1ce668523faae821ec62689911a1fe476855c9
Vladislav <itochanenko@gmail.com>
2020-04-24 22:15:10 +0300

    Add project runner

--- LabRasp1/Lab 9/Part 5/Program.cs
+++ LabRasp1/Lab 9/Part 5/Program.cs
@@ -3,7 +3,15 @@
 namespace Part_5 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            var c = new Calculator("./resources/index.html");
+            c.AddTask(100);
+            c.AddTask(500);
+            c.AddTask(1000);
+            c.AddTask(1500);
+            c.AddTask(2000);
+            c.AddTask(2500);
+            c.AddTask(3000);
+            c.Run();
         }
     }
 }
